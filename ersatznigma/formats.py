cd94422b38c2d0405f1cfbcec6e7343df0c89212
tnalpgge <tony.k.github.59cc00@nog.net>
2015-12-31 00:12:38 -0800

    Add E06 message formats

--- ersatznigma/formats.py
+++ ersatznigma/formats.py
@@ -28,7 +28,7 @@ class ESingleMessage(Format):
         return self.intro() + self.preamble() + self.message() + self.outro()
 
     def outro(self):
-            return [ [ '0' ] * 3 ] * 2
+            return [ self.int2group(0, pad=3) ] * 2
 
 class ENonTraffic(Format):
     def __init__(self, schedule_id):
@@ -36,6 +36,101 @@ class ENonTraffic(Format):
 
     def generate(self):
         return ( [ self.int2group(self.schedule_id) ] * 3 + [ self.int2group(0, pad=3) ] ) * int(120 / 16)
+
+class E06NonTraffic(ENonTraffic):
+    """
+    http://priyom.org/number-stations/english/e06
+    """
+    def __init__(self):
+        super().__init__(376)
+
+    def generate(self):
+        return ( [ self.int2group(self.schedule_id) ] * 3 + [ self.int2group(0, pad=5) ] ) * int(240 / 18)
+
+class E06SingleMessage(ESingleMessage):
+    """
+    http://priyom.org/number-stations/english/e06
+    """
+    def __init__(self, slope, intercept):
+        super().__init__(460)
+        self.unknown = int(random.random() * 1000)
+        self.groupcount = int(random.random() * slope) + intercept
+
+    def generate(self):
+        return self.intro() + self.preamble() + self.message() + self.postamble() + self.outro()
+
+    def intro(self):
+        return [ self.int2group(self.schedule_id) ] * 60
+
+    def preamble(self):
+        return [ self.int2group(self.unknown, pad=3) ] * 2 + [ self.int2group(self.groupcount) ] * 2
+
+    def message(self):
+        m = list()
+        for i in range(self.groupcount):
+            j = self.int2group(int(random.random() * 100000), pad=5)
+            m.append(j)
+            m.append(j)
+        return m
+
+    def postamble(self):
+        return self.preamble()
+
+    def outro(self):
+        return [ self.int2group(0, pad=5) ]
+
+class E06DoubleMessage(Format):
+    """
+    http://priyom.org/number-stations/english/e06
+    """
+    def __init__(self, slope, intercept):
+        super().__init__(509)
+        self.unknown = [ int(random.random() * 1000), int(random.random() * 1000) ]
+        self.groupcount = [ int(random.random() * slope) + intercept, int(random.random() * slope) + intercept ]
+
+    def generate(self):
+        return self.first_intro() + self.first_preamble() + self.first_message() + self.first_postamble() + self.second_intro() + self.second_preamble() + self.second_message() + self.second_postamble() + self.outro()
+
+    def intro(self, duration):
+        return [ self.int2group(self.schedule_id) ] * int(duration / 4)
+
+    def first_intro(self):
+        return self.intro(240)
+
+    def second_intro(self):
+        return self.intro(60)
+
+    def preamble(self, n):
+        return [ self.int2group(self.unknown[n]) ] * 2 + [ self.int2group(self.groupcount[n]) ] * 2
+
+    def first_preamble(self):
+        return self.preamble(0)
+
+    def second_preamble(self):
+        return self.preamble(1)
+
+    def first_postamble(self):
+        return self.first_preamble()
+
+    def second_postamble(self):
+        return self.second_preamble()
+
+    def message(self, n):
+        m = list()
+        for i in range(self.groupcount[n]):
+            j = self.int2group(int(random.random() * 100000), pad=5)
+            m.append(j)
+            m.append(j)
+        return m
+
+    def first_message(self):
+        return self.message(0)
+
+    def second_message(self):
+        return self.message(1)
+
+    def outro(self):
+        return [ self.int2group(0, pad=5) ]
 
 class E07aNonTraffic(ENonTraffic):
     """
@@ -133,4 +228,4 @@ class E07DoubleMessage(Format):
         return self.message(1)
 
     def outro(self):
-        return [ [ '0' ] * 3 ] * 2
+        return [ self.int2group(0, pad=3) ] * 2
